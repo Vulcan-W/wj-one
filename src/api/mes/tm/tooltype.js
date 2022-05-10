244d61416d781834634ac782488346035e124386
JinLu.Yin <411641505@qq.com>
2022-05-11 00:33:42 +0800

    工装夹具类型设置

---
+++ src/api/mes/tm/tooltype.js
@@ -0,0 +1,44 @@
+import request from '@/utils/request'
+
+// 查询工装夹具类型列表
+export function listTooltype(query) {
+  return request({
+    url: '/mes/tm/tooltype/list',
+    method: 'get',
+    params: query
+  })
+}
+
+// 查询工装夹具类型详细
+export function getTooltype(toolTypeId) {
+  return request({
+    url: '/mes/tm/tooltype/' + toolTypeId,
+    method: 'get'
+  })
+}
+
+// 新增工装夹具类型
+export function addTooltype(data) {
+  return request({
+    url: '/mes/tm/tooltype',
+    method: 'post',
+    data: data
+  })
+}
+
+// 修改工装夹具类型
+export function updateTooltype(data) {
+  return request({
+    url: '/mes/tm/tooltype',
+    method: 'put',
+    data: data
+  })
+}
+
+// 删除工装夹具类型
+export function delTooltype(toolTypeId) {
+  return request({
+    url: '/mes/tm/tooltype/' + toolTypeId,
+    method: 'delete'
+  })
+}
