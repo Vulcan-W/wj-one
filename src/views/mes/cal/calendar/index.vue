a8981eb2a419a83fdfd79d1e55ec826f86d6edf1
JinLu.Yin <411641505@qq.com>
2022-06-08 23:29:27 +0800

    排班日历

---
+++ src/views/mes/cal/calendar/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="app-container">
+        <el-tabs type="card">
+            <el-tab-pane label="类型"></el-tab-pane>
+            <el-tab-pane label="班组"></el-tab-pane>
+            <el-tab-pane label="个人"></el-tab-pane>
+        </el-tabs>
+    </div>
+</template>
+
+<script>
+
+
+export default {
+    data(){
+        return {
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
