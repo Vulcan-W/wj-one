0a97eea49879e9e230ba03bb0e2b843b671f1ab0
JinLu.Yin <411641505@qq.com>
2022-06-16 21:04:47 +0800

    按钮权限控制

--- src/views/mes/dv/subject/index.vue
+++ src/views/mes/dv/subject/index.vue
@@ -41,7 +41,7 @@
           icon="el-icon-plus"
           size="mini"
           @click="handleAdd"
-          v-hasPermi="['dv:dvsubject:add']"
+          v-hasPermi="['mes:dv:dvsubject:add']"
         >新增</el-button>
       </el-col>
       <el-col :span="1.5">
@@ -52,7 +52,7 @@
           size="mini"
           :disabled="single"
           @click="handleUpdate"
-          v-hasPermi="['dv:dvsubject:edit']"
+          v-hasPermi="['mes:dv:dvsubject:edit']"
         >修改</el-button>
       </el-col>
       <el-col :span="1.5">
@@ -63,7 +63,7 @@
           size="mini"
           :disabled="multiple"
           @click="handleDelete"
-          v-hasPermi="['dv:dvsubject:remove']"
+          v-hasPermi="['mes:dv:dvsubject:remove']"
         >删除</el-button>
       </el-col>
       <el-col :span="1.5">
@@ -73,7 +73,7 @@
           icon="el-icon-download"
           size="mini"
           @click="handleExport"
-          v-hasPermi="['dv:dvsubject:export']"
+          v-hasPermi="['mes:dv:dvsubject:export']"
         >导出</el-button>
       </el-col>
       <right-toolbar :showSearch.sync="showSearch" @queryTable="getList"></right-toolbar>
@@ -103,14 +103,14 @@
             type="text"
             icon="el-icon-edit"
             @click="handleUpdate(scope.row)"
-            v-hasPermi="['dv:dvsubject:edit']"
+            v-hasPermi="['mes:dv:dvsubject:edit']"
           >修改</el-button>
           <el-button
             size="mini"
             type="text"
             icon="el-icon-delete"
             @click="handleDelete(scope.row)"
-            v-hasPermi="['dv:dvsubject:remove']"
+            v-hasPermi="['mes:dv:dvsubject:remove']"
           >删除</el-button>
         </template>
       </el-table-column>
